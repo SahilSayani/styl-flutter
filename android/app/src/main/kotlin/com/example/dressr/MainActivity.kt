fdb9802648f2817ba5139fe529ffae4abf905128
SahilSayani <sahilsayani7@gmail.com>
2022-09-02 13:28:00 +0530

    set up the app

---
+++ android/app/src/main/kotlin/com/example/dressr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dressr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+          title: const Text(
+            "Homepage",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: const Text(
+            "body",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+         ),
+     );
+  }
+  }
+
